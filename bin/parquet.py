14122f9fdef676cf59119f900886aad2bd19cd67
David Roher <david.roher@gmail.com>
2023-01-10 23:55:24 -0500

    Fix Arrow Parquet conversion bug

--- bin/parquet.py
+++ bin/parquet.py
@@ -5,7 +5,7 @@ from pyarrow import csv, parquet
 
 def file_to_data_frame_to_parquet(local_file: str, parquet_file: str) -> None:
     table = csv.read_csv(local_file, convert_options=csv.ConvertOptions(strings_can_be_null=True))
-    parquet.write_table(table, parquet_file, compression='zstd')
+    parquet.write_table(table, parquet_file, compression='zstd', write_batch_size=10000000)
 
 
 if __name__ == "__main__":
