37ffb5e838e37fcb353a204bfb30f19836b978f9
David Roher <david.roher@gmail.com>
2023-05-25 13:52:48 -0400

    Use delta compression for event_key

--- bin/parquet.py
+++ bin/parquet.py
@@ -4,9 +4,19 @@ from pyarrow import csv, parquet
 
 
 def file_to_data_frame_to_parquet(local_file: str, parquet_file: str) -> None:
-    table = csv.read_csv(local_file, convert_options=csv.ConvertOptions(strings_can_be_null=True))
-    parquet.write_table(table, parquet_file, compression='zstd', row_group_size=1000000000, write_batch_size=1000000000)
-
+    explicit_types = {
+        "event_key": "int32",
+    }
+    table = csv.read_csv(local_file,
+                         convert_options=csv.ConvertOptions(strings_can_be_null=True,
+                                                            column_types=explicit_types))
+    if "event" in local_file:
+        table = table.sort_by("event_key")
+    parquet.write_table(table,
+                        parquet_file,
+                        compression='zstd',
+                        use_dictionary = [c for c in table.column_names if c != "event_key"],
+                        column_encoding={'event_key': 'DELTA_BINARY_PACKED'})
 
 if __name__ == "__main__":
     for f in glob.glob("csv/*.csv"):
